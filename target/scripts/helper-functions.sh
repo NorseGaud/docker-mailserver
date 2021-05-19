62878fdd0e282a96ad24bca775dd0321685e5d90
NorseGaud <connarpierce@gmail.com>
2021-05-19 08:37:54 -0400

    lint

--- target/scripts/helper-functions.sh
+++ target/scripts/helper-functions.sh
@@ -15,21 +15,19 @@ function escape
   echo "${1//./\\.}"
 }
 
-
 function create_lock
 {
 	LOCK_FILE="/tmp/docker-mailserver/$1.lock"
 	RETRIES=0
 	while [[ -e "${LOCK_FILE}" ]]
 	do
-		[ ${RETRIES} -ge 15 ] && echo "Too many attempts to lock ${LOCK_FILE}..." && exit 100
+		[ "${RETRIES}" -ge 15 ] && echo "Too many attempts to lock ${LOCK_FILE}..." && exit 100
 		sleep 5
 		((RETRIES+=1))
 	done
 	trap remove_lock EXIT
 	touch "${LOCK_FILE}"
 }
-
 
 function remove_lock
 {
